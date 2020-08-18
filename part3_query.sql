d791f0eed67ec5a19b8cc01cf2cbe823eaf01f6f
Shreyas Walvekar <shreyas.whis@gmail.com>
2020-08-18 15:29:23 +1000

    Update part3_query.sql
    Changed query logic for time till resolution to be from Open to Resoved

--- part3_query.sql
+++ part3_query.sql
@@ -8,7 +8,9 @@ SELECT a1.ticket_id,
        (a2.performed_at - a1.performed_at) / 3600      AS time_spent_open,
        (a3.performed_at - a2.performed_at) / 3600      AS time_spent_waiting_on_customer,
        (a5.performed_at - a4.performed_at) / 3600      AS time_spent_waiting_for_response,
-       (a6.performed_at - a5.performed_at) / 3600      AS time_till_resolution,
+       (a5.performed_at - a1.performed_at) / 3600      AS time_till_resolution,
+       -- a6 definition is removed as it is not required, time till resolution was updated to be time from open to Response
+       -- (a6.performed_at - a5.performed_at) / 3600      AS time_till_resolution,
        -- aggregate function on table to get the earliest activity with 'contacted' as true
        (MIN(a7.performed_at) - a1.performed_at) / 3600 as time_to_first_response
 FROM activity a1
@@ -17,14 +19,14 @@ FROM activity a1
          INNER JOIN activity a3 ON a3.ticket_id = a1.ticket_id
          INNER JOIN activity a4 ON a4.ticket_id = a1.ticket_id
          INNER JOIN activity a5 ON a5.ticket_id = a1.ticket_id
-         INNER JOIN activity a6 ON a6.ticket_id = a1.ticket_id
+         -- INNER JOIN activity a6 ON a6.ticket_id = a1.ticket_id
          INNER JOIN activity a7 ON a7.ticket_id = a1.ticket_id
 WHERE a1.status_enum = (SELECT id from enum_status WHERE status_type = 'Open')
   AND a2.status_enum = (SELECT id from enum_status WHERE status_type = 'Waiting for Customer')
   AND a3.status_enum = (SELECT id from enum_status WHERE status_type = 'Waiting for Third Party')
   AND a4.status_enum = (SELECT id from enum_status WHERE status_type = 'Pending')
   AND a5.status_enum = (SELECT id from enum_status WHERE status_type = 'Resolved')
-  AND a6.status_enum = (SELECT id from enum_status WHERE status_type = 'Closed')
+  -- AND a6.status_enum = (SELECT id from enum_status WHERE status_type = 'Closed')
   -- once all the status are done, get the rows with when the customer has been contacted
   AND a7.contacted_customer = true
 -- Group required for aggregate function, MIN
