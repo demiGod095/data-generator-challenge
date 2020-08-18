6d85b1e8cc33a9607aa2e200d68a3b373dbe15bc
Shreyas Walvekar <shreyas.whis@gmail.com>
2020-08-18 14:32:20 +1000

    Added readme file

--- part4_script.sh
+++ part4_script.sh
@@ -1,7 +1,8 @@
 # this script calls the other scripts one by one
 
 # Ensure python requirements are installed prior to use
-python -m pip install -r requirements.txt --user
+# commented out as redundant in readme for project setup
+# python -m pip install -r requirements.txt --user
 
 # run ticket generator
 python part1_ticketGen.py -n 1000 -o activities.json
@@ -13,4 +14,5 @@ python part2_create_db.py -o db.sqlite3
 python part2_load_json2db.py -i activities.json -o db.sqlite3
 
 # query the database, by loading the db and running the query from file
+# need to have sqlite3 installed as specified in readme file
 sqlite3 db.sqlite3 < part3_query.sql
